ee77bc401546bd112f5defab1787644f76544bcd
Mohit sahu <mohitsahu60067@gmail.com>
2025-03-03 17:57:20 +0530

    Feat: Added API route structure and user routes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import { StatusCodes } from 'http-status-codes';
 
 import { connectDB } from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import apiRouter from './routes/apieRoutes.js'
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({
     message: 'pong'
   });
 });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
